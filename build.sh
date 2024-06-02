2fc47c1b02c5f1e5cd2f79fc0999a6697ba694fe
Jaden Down <24631328+jpdown@users.noreply.github.com>
2024-06-02 16:48:43 -0500

    port bazzite waydroid files

--- build.sh
+++ build.sh
@@ -24,3 +24,18 @@ RELEASE="$(rpm -E %fedora)"
 
 # Install driverctl (TODO can i set up the pci-stub in this repo)
 rpm-ostree install driverctl
+
+
+# Install Waydroid
+curl -Lo /etc/yum.repos.d/_copr_kylegospo-bazzite.repo https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite/repo/fedora-"${FEDORA_MAJOR_VERSION}"/kylegospo-bazzite-fedora-"${FEDORA_MAJOR_VERSION}".repo
+rpm-ostree install waydroid
+# Bazzite does this so I will do it too
+sed -i~ -E 's/=.\$\(command -v (nft|ip6?tables-legacy).*/=/g' /usr/lib/waydroid/data/scripts/waydroid-net.sh
+
+# Enable the Bazzite Waydroid Justfile
+echo "import \"/usr/share/ublue-os/just/82-bazzite-waydroid.just\"" >> /usr/share/ublue-os/justfile
+
+systemctl enable waydroid-workaround.service
+systemctl disable waydroid-container.service
+curl -Lo /usr/bin/waydroid-choose-gpu https://raw.githubusercontent.com/KyleGospo/waydroid-scripts/main/waydroid-choose-gpu.sh
+chmod +x /usr/bin/waydroid-choose-gpu
